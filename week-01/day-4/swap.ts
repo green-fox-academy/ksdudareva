4b85e9cb1db5ecbf96570337355984713f0a99ea
ksdudareva <ksdudareva@gmail.com>
2019-09-12 17:07:50 +0200

    add swap

---
+++ week-01/day-4/swap.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// Swap the values of these variables
+let a: number = 123;
+let b: number = 526;
+
+console.log(a);
+console.log(b);
+
+let temp: number = a;
+
+a = b;
+
+b = temp;
+
+console.log(a);
+console.log(b);
