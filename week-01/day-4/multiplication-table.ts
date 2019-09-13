ce79f9792b7c1eca4e953ba68e40595eb49085eb
ksdudareva <ksdudareva@gmail.com>
2019-09-13 12:42:38 +0200

    solved

---
+++ week-01/day-4/multiplication-table.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let number: number = 15;
+let multiply: number = 1;
+
+for (multiply=1; multiply <=10; multiply++) {
+    console.log(multiply + " * " + number + " = " + (multiply*number));
+
+}
+
