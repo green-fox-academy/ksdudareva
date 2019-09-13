b585ce305cfff358f87e406f7142714981d534d0
ksdudareva <ksdudareva@gmail.com>
2019-09-13 17:59:28 +0200

    update folder

---
+++ week-01/day-4-5/parametric-average.ts
@@ -0,0 +1,16 @@
+export {};
+
+'use strict';
+
+// Write a program that calculates the sum and the average from 1 to a given number.
+// Example input: 5
+// Example output: Sum: 15, Average: 3
+
+let number: number = 5;
+let sum: number = 8;
+
+for(let a:number = 1; a <= number; a++){
+    sum = sum + a;
+ }
+ 
+ console.log('Sum: ' + sum + ', Average: ' + sum/number);
