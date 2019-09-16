d762bd350c2c5ea60b07fe8efd9e6b234bb7974a
ksdudareva <ksdudareva@gmail.com>
2019-09-16 17:31:43 +0200

    added sum all

--- week-02/day-1/sum-all.ts
+++ week-02/day-1/sum-all.ts
@@ -6,3 +6,11 @@ export {};
 // -  Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
 // -  Log the sum of the elements in `ai` to the console
 
+let ai: number[] = [3, 4, 5, 6, 7];
+let aiSum: number = 0;
+
+ai.forEach(function(i) {
+    aiSum = aiSum + i;
+})
+console.log(aiSum);
+
