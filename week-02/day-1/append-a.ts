48d60d887dc8a02943927ec2865c777bfa0a48d9
ksdudareva <ksdudareva@gmail.com>
2019-09-16 16:36:05 +0200

    added double items

---
+++ week-02/day-1/append-a.ts
@@ -0,0 +1,13 @@
+export {};
+
+'use strict';
+
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+let animals: string[] = ["koal", "pand", "zebr"];
+
+animals.forEach(function(item) {
+    console.log(item + "a");
+});
---
+++ week-02/day-1/change-element.ts
@@ -0,0 +1,18 @@
+export {};
+
+'use strict';
+
+// -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method 
+// -  Print the fourth element as a test
+
+let numList:number [] = [1, 2, 3, 8, 5, 6];
+
+numList = numList.map(function change (i) {
+    if (i == 8) {
+        return 4;
+    } else return i;
+}
+)
+console.log(numList[3]);
+
