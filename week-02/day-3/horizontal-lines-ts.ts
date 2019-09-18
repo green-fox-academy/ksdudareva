75b579c02b59663cb814bbeb99f9cf81d3589d57
ksdudareva <ksdudareva@gmail.com>
2019-09-18 17:08:08 +0200

    ugh

---
+++ week-02/day-3/horizontal-lines-ts.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Create a function that draws a single line and takes 2 parameters:
+// the x and y coordinates of the line's starting point
+// and draws a 50 long horizontal line from that point.
+// Draw at least 3 lines with that function using a loop.\
+
+function drawLine(x: number, y: number) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x+50, y);
+    ctx.stroke();}
+
+    for (let e=10; e <= canvas.height; e+=50) {
+        drawLine(0, e)
+    }
