d028f89343ccfbf8d5371f307c2fa6924e6fc641
ksdudareva <ksdudareva@gmail.com>
2019-09-20 16:18:24 +0200

    THE BEST STAR EVER

--- week-02/day-5/envelope-star.ts
+++ week-02/day-5/envelope-star.ts
@@ -7,13 +7,13 @@ const ctx = canvas.getContext('2d');
 
 function envelope(x, y, xx, yy) {
     ctx.beginPath();
-    //ctx.strokeStyle = "#34c6eb";
+    ctx.strokeStyle = "#34c6eb";
     ctx.moveTo(x, y);
     ctx.lineTo(xx, yy);
     ctx.stroke();
 }
 
-for (let a = 0; a < canvas.height / 2; a += 20) {
+for (let a = 20; a < canvas.height / 2; a += 20) {
     ctx.strokeStyle = "blue"
     envelope(canvas.height / 2, canvas.height / 2 - a, a, canvas.height / 2);
 }
@@ -23,12 +23,12 @@ for (let a = 0; a < canvas.height / 2; a += 20) {
     envelope(canvas.height / 2 + a, canvas.height / 2, canvas.height / 2, a);
 }
 
-for (let a = 0; a < canvas.height / 2; a += 20) {
+for (let a = 20; a < canvas.height / 2; a += 20) {
     ctx.strokeStyle = "black"
     envelope(canvas.height / 2, canvas.height / 2 + a, a, canvas.height / 2);
 }
 
-for (let a = 0; a < canvas.height / 2; a += 20) {
+for (let a = 20; a <= canvas.height / 2; a += 20) {
     ctx.strokeStyle = "red"
     envelope(canvas.height / 2, canvas.height / 2 + a, canvas.height - a, canvas.height/2)
 }
