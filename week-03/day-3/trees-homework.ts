f0120f942f6fa638a7c1367618171693053a1827
ksdudareva <ksdudareva@gmail.com>
2019-09-25 00:03:33 +0200

    adding day3 homework

---
+++ week-03/day-3/trees-homework.ts
@@ -0,0 +1,35 @@
+let chestnut = {
+    type: 'chestnut tree',
+    leafColor: 'red',
+    age: '50',
+    sex: 'male'
+};
+
+let walnut = {
+    type: 'walnut tree',
+    leafColor: 'brown',
+    age: '100',
+    sex: 'female'
+};
+
+let maple = {
+    type: 'maple tree',
+    leafColor: 'red',
+    age: '1',
+    sex: 'female'
+
+};
+
+let fig = {
+    type: 'fig tree',
+    leafColor: 'green',
+    age: '15',
+    sex: 'female'
+};
+
+let willow = {
+    type: 'willow tree',
+    leafColor: 'green',
+    age: '30',
+    sex: 'male'
+}
