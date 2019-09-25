4c7394cfc322f7f79868d3bd23ea732c6b1b2706
ksdudareva <ksdudareva@gmail.com>
2019-09-25 09:33:05 +0200

    adding doubled.ts

--- week-03/day-2/doubled/doubled.ts
+++ week-03/day-2/doubled/doubled.ts
@@ -4,12 +4,21 @@ const fs = require('fs');
 
 let text = fs.readFileSync('duplicated-chars.txt', 'utf-8');
 let textLines: string[] = text.split('\n');
-let textLetters: string [][] = textLines.map(function (e) {
-    return e.split('');
-})
+let newLines: string = '';
+let newText: string = ''
+let charArray: string [];
 
-for (let i: number = 0; i < textLetters.length; i++){
-    for(let j:number = 0; j < textLetters[i].length; j++) {
-        if 
+for (let i: number = 0; i < textLines.length; i++) {
+    newText = textLines[i];
+    charArray = newText.split("");
+    for(let j = 1; j < charArray.length; j++){
+        charArray.splice(j,1);
     }
+    newLines += charArray.join("") + "\n";
 }
+
+console.log(newLines);
+
+
+//создать стринг
+//в лупе добавлять каждую вторую букву
