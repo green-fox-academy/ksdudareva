2ee7ddd114c7ecf43b5f58192eb952b1e3b8237c
ksdudareva <ksdudareva@gmail.com>
2019-10-22 15:36:36 +0200

    content-1 done

---
+++ week-06/day-2/content/content-1.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//     1)  Fill every paragraph with the last one's content.
+//      Commented out 
+
+// let lastParagraph = document.querySelector('.animals').textContent;
+// let everyParagraph = document.querySelectorAll('p');
+// for (let i = 0; i < everyParagraph.length; i++) {
+//     everyParagraph[i].textContent = lastParagraph;
+// }
+
+//     2)  Do the same again, but you should keep the cat strong.
+
+let lastParagraph = document.querySelector('.animals').innerHTML;
+let everyParagraph = document.querySelectorAll('p');
+for (let i = 0; i < everyParagraph.length; i++) {
+    everyParagraph[i].innerHTML = lastParagraph;
+}
