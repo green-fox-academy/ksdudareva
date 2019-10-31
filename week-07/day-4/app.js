096792afa28f7387e6d94480da7bbefa381f55bf
ksdudareva <ksdudareva@gmail.com>
2019-10-31 17:19:49 +0100

    end of day commit - reddit not finished

--- week-07/day-4/app.js
+++ week-07/day-4/app.js
@@ -3,15 +3,17 @@ const mysql = require('mysql');
 const path = require('path');
 const databaseName = "reddit";
 const tableName = "posts";
+const bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 const app = express();
 const PORT = 8080;
 
 var conn = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : databaseName
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: databaseName
 });
 
 conn.connect((err) => {
@@ -20,7 +22,7 @@ conn.connect((err) => {
         return;
     }
     console.log('Connected!');
-  });
+});
 
 app.use(express.json());
 
@@ -32,22 +34,33 @@ app.use(express.json());
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
-  });
+});
 
 let output = {};
 
 app.get('/posts', (req, res) => {
-    conn.query('SELECT * FROM posts;', function(err, rows) {
+    conn.query('SELECT * FROM posts;', function (err, rows) {
         if (err) {
-          res.status(500).send('Database error');
-          return;
+            res.status(500).send('Database error');
+            return;
         }
-        output = {"posts": rows};
-    res.status(200);
-    res.send(output);
-})
+        output = { "posts": rows };
+    })
+    res.status(200).send(output);
 });
-  
+
+app.post('/posts', jsonParser, (req, res) => {
+    conn.query('INSERT INTO posts (title, url) VALUES (' + req.body.title + ', ' + req.body.url + ');', function (err, rows) {
+        if (err) {
+            res.status(500).send('Database error');
+            return;
+        }
+        
+    })
+    res.status(200).send(output);
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`The server is up and running on ${PORT}`);
