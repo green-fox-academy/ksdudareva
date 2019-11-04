e3985d30644db320fe4793067c233eb3f0754992
ksdudareva <ksdudareva@gmail.com>
2019-11-04 11:21:58 +0100

    add upvote and downvote

--- week-07/day-4/app.js
+++ week-07/day-4/app.js
@@ -26,41 +26,58 @@ conn.connect((err) => {
 
 app.use(express.json());
 
-
-
-// create a simple get endpoint for /hello
-// it should return the string hello world
-// start your server and check if it works
-
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 let output = {};
-
 app.get('/posts', (req, res) => {
     conn.query('SELECT * FROM posts;', function (err, rows) {
         if (err) {
             res.status(500).send('Database error');
             return;
         }
-        output = { "posts": rows };
     })
+    output = { "posts": rows };
     res.status(200).send(output);
 });
 
 app.post('/posts', jsonParser, (req, res) => {
-    conn.query('INSERT INTO posts (title, url) VALUES (' + req.body.title + ', ' + req.body.url + ');', function (err, rows) {
+    conn.query('INSERT INTO posts (title, url) VALUES ("' + req.body.title + '", "' + req.body.url + '");', function (err, rows) {
         if (err) {
             res.status(500).send('Database error');
             return;
         }
-        
     })
+    output = "Posted!";
     res.status(200).send(output);
 });
 
+app.put('/posts/:id/upvote', (req, res) => {
+    let output;
+    conn.query('UPDATE posts SET score = score + 1 WHERE id = ?;',[req.params.id], function (err, rows) {
+        if (err) {
+            res.status(500).send('Database error');
+            return;
+        }
+    })
+    output = "Upvoted!";
+    res.status(200).send(output);
+})
 
+// select * from table where id = ? & name = ?, [4, 'aboud'], (err, rows) // SQL injection
+
+app.put('/posts/:id/downvote', (req, res) => {
+    let output;
+    conn.query('UPDATE posts SET score = score - 1 WHERE id = ?;', [req.params.id], function (err, rows) {
+        if (err) {
+            res.status(500).send('Database error');
+            return;
+        }
+    })
+    output = "Downvoted :(";
+    res.status(200).send(output);
+})
 
 app.listen(PORT, () => {
     console.log(`The server is up and running on ${PORT}`);
