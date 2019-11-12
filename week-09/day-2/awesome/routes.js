b97d38a23214389e0701aef488a0025c9ccfea3c
ksdudareva <ksdudareva@gmail.com>
2019-11-12 16:37:15 +0100

    commit

--- week-09/day-2/awesome/routes.js
+++ week-09/day-2/awesome/routes.js
@@ -11,12 +11,12 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 let mixList = [
-    { "author": "Blue Swede", "title": "Hooked on a Feeling", "genre": "pop", "year": 1968, "rating:": 4.1 },
-    { "author": "The Jackson 5", "title": "I Want You Back", "genre": "pop", "year": 1969, "rating:": 5 },
-    { "author": "The Runaways", "title": "Cherry Bomb", "genre": "rock", "year": 1976, "rating:": 4.6 },
-    { "author": "Electric Light Orchestra", "title": "Mr. Blue Sky", "genre": "rock", "year": 1977, "rating:": 4.9 },
-    { "author": "Fleetwood Mac", "title": "The Chain", "genre": "rock", "year": 1977, "rating:": 3.9 },
-    { "author": "Alla Pugacheva", "title": "Million Roses", "genre": "pop", "year": 1983, "rating:": 2.0 },
+    { "id": 1, "author": "Blue Swede", "title": "Hooked on a Feeling", "genre": "pop", "year": 1968, "rating": 4.1 },
+    { "id": 2, "author": "The Jackson 5", "title": "I Want You Back", "genre": "pop", "year": 1969, "rating": 5 },
+    { "id": 3, "author": "The Runaways", "title": "Cherry Bomb", "genre": "rock", "year": 1976, "rating": 4.6 },
+    { "id": 4, "author": "Electric Light Orchestra", "title": "Mr. Blue Sky", "genre": "rock", "year": 1977, "rating": 4.9 },
+    { "id": 5, "author": "Fleetwood Mac", "title": "The Chain", "genre": "rock", "year": 1977, "rating": 3.9 },
+    { "id": 6, "author": "Alla Pugacheva", "title": "Million Roses", "genre": "pop", "year": 1983, "rating": 2.0 },
 ]
 
 app.get('/awesome', (req, res) => {
@@ -24,15 +24,38 @@ app.get('/awesome', (req, res) => {
     res.send(output);
 });
 
-app.post('awesome/add', (req, res) => {
-    let newSong = {
-        "author": "Blue Swede",
-        "title": "Hooked on a Feeling",
-        "genre": "pop",
-        "year": 1968,
-        "rating:": 4.1
-    };
+app.post('/awesome/add', (req, res) => {
+    let newSong = req.body;
+    // let newSong = {
+    //     "author": "Blue Swede",
+    //     "title": "Hooked on a Feeling",
+    //     "genre": "pop",
+    //     "year": 1968,
+    //     "rating:": 4.1
+    // };
+    let maxID = Math.max.apply(Math, mixList.map(function (o) { return o.id; }));
+    newSong.id = maxID + 1;
+    mixList.push(newSong);
+    let output = newSong;
+    res.send(output);
+});
 
+let deleted = [];
+
+app.delete('/awesome/delete/:id', (req, res) => {
+    let id = req.params.id;
+    let index = mixList.map((x) => x.id).indexOf(parseInt(id));
+    let output = mixList.splice(index, 1);
+    deleted.push(output);
+    res.send(output);
+});
+
+app.put('/awesome/rating/:id', (req, res) => {
+    let id = req.params.id;
+    let index = mixList.map((x) => x.id).indexOf(parseInt(id));
+    mixList[index].rating = req.query.rating;
+    let output = mixList[index];
+    res.send(output);
 })
 
 module.exports = app;
