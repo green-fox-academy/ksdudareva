69aef6a14af43852a9b99052a1d89675645850c2
ksdudareva <ksdudareva@gmail.com>
2019-11-25 17:31:40 +0100

    foxplayer - continued backend

--- week-10/Foxplayer/backend.js
+++ week-10/Foxplayer/backend.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const path = require('path');
 const bodyParser = require('body-parser');
+const env = require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
@@ -29,9 +30,46 @@ app.get('/', (req, res) => {
 });
 
 
+// GET /playlists
+app.get('/playlists', (req, res) => {
+    conn.query('SELECT * FROM playlists;', function (err, playlists) {
+        res.status(200).send(playlists);
+    });
+});
+
+// GET /playlist-tracks
+
+
+
+// GET /playlist-tracks/:playlist_id
+
+
+
+// add playlist
+
+app.post('/playlists', (req, res) => {
+    let newPlaylist = req.body.title;
+    conn.query('CREATE TABLE ? (`id` INT unsigned NOT NULL AUTO_INCREMENT, `title` VARCHAR(255) NOT NULL, `artist` VARCHAR(255) NOT NULL,`duration` FLOAT unsigned NOT NULL, `path` VARCHAR(255) NOT NULL, PRIMARY KEY (`id`));', [newPlaylist], function(err, rows) {
+        conn.query('INSERT INTO playlists VALUES (null, ?, default)', [newPlaylist], function(err, newRow) {
+            conn.query('SELECT * FROM playlists', function(err, playlists){
+                res.send(playlists);
+            });
+        });
+    });
+})
+
+// delete playlist
+
+app.delete('/playlists/:id', (req, res) => {
+    conn.query('DELETE FROM playlists WHERE id = ?', [req.params.id], function (err, rows){
+
+    })
+})
 
 //listen to port
 
 app.listen(PORT, () => {
     console.log(`The server is up and running on ${PORT}`);
 });
+
+
